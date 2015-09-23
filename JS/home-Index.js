c4642cc82eeb9daae43d8275e9aa68caae695e10
gtl-mihir <mihir.joshi@gatewaytechnolabs.com>
2015-09-23 17:36:38 +0530

    First Commit

---
+++ JS/home-Index.js
@@ -0,0 +1,16 @@
+﻿//home-index
+
+var app = angular.module("myApp", []);
+app.controller("homeIndexController", function ($scope) {
+    $scope.username = "";
+    $scope.password = "";
+    $scope.myBtnClick = function () {
+       
+        if ($scope.username == "nosql" && $scope.password == "html5") {
+            alert("welcome to the hell");
+        } else {
+            alert("Invalid Password");
+        }
+        //$scope.myVar = !$scope.myVar;
+    };
+});
